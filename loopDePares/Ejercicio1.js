4b7026ac2bd75e3bdef1ec0f942f2bb191138209
Silvana Cuadra <silvanagcuadra@gmail.com>
2021-06-14 21:17:04 -0300

    Actualización de la consigna

--- loopDePares/Ejercicio1.js
+++ loopDePares/Ejercicio1.js
@@ -1,4 +1,4 @@
-// Ejercicio 1 - Crear una función llamada loopDePares() que reciba como parámetro un número y haga un loop de 0 a 100 mostrando en la consola cada número del loop. En caso de que el número de la iteranción sumado con el número pasado por parámetro sea par, mostrár por consola "el número x es par"
+// Ejercicio 1 - Crear una función llamada loopDePares() que reciba como parámetro un número y haga un loop de 0 a 100. En caso de que el número de la iteranción sumado con el número pasado por parámetro sea par, mostrár por consola "el número x es par"
 
 const loopDePares = (n) => {
     for (let i = 0; i <= 100; i++) {
@@ -7,7 +7,7 @@ const loopDePares = (n) => {
             console.log(`El número ${numero} es par`);
         }
         else {
-            console.log(numero);
+            continue;
         }
     }
 }
